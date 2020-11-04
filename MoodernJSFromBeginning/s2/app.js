2970b57d780b4e7e9412f0578001494a23915269
Yan <diteric@gmail.com>
2020-11-04 21:56:10 +0000

    JS console

---
+++ MoodernJSFromBeginning/s2/app.js
@@ -0,0 +1,20 @@
+console.log('object');
+console.log(123);
+console.log(true);
+
+var greeting = 'Hello';
+console.log(greeting);
+console.log([1,2,3,5]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+
+console.error('this is an error');
+console.warn('this is an warn');
+// console.clear();
+
+console.time('Hello')
+  console.log(123);
+  console.log(123);
+  console.log(123);
+  console.log(123);
+console.timeEnd('Hello')
---
+++ MoodernJSFromBeginning/s2/index.js
@@ -0,0 +1,10 @@
+let name = 'Yan';
+console.log(name);
+
+let firstName = 'Yan'; 
+let FirstNmae = 'Yan2';
+
+const interestRate = 0.3;
+console.log('fe');
+console.log("abc");
+
