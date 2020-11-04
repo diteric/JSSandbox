6c55bf121a38bc7daf3a7b8d34dbc1c9efa4d49b
Yan <diteric@gmail.com>
2020-11-04 23:00:56 +0000

    Math Object

---
+++ MoodernJSFromBeginning/s2/calc.js
@@ -0,0 +1,18 @@
+const num1 = 100;
+const num2 = 50;
+
+let val;
+val = num1 + num2;
+console.log(val);
+
+// Math Object
+val = Math.PI;
+val = Math.round(2.4);
+val = Math.ceil(2.4);
+val = Math.floor(2.4);
+
+val = Math.min(2,34,67,89,3,4);
+
+val = Math.floor(Math.random() * 20 + 1); // from 1 to 20
+
+console.log(val);
