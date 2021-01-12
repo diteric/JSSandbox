b65be224fff85dc1f36ad820a0a4f5ad85271abf
Yan <diteric@gmail.com>
2021-01-12 12:21:10 +0000

    6- oop book list - add an alert for adding a book

--- MoodernJSFromBeginning/s6-booklist/app.js
+++ MoodernJSFromBeginning/s6-booklist/app.js
@@ -1,3 +1,6 @@
+// highlights
+
+
 function Book(title, auther, isbn){
   this.title = title;
   this.auther = auther;
@@ -9,18 +12,36 @@ UI.prototype.addBookToTable = function(book) {
 	// get the book table html element
 	bookTable = document.getElementById('book-list');
 	newBook = document.createElement('tr');
-	newBook.innerHTML = `
+	newBook.innerHTML = ` 
 		<td>${book.title}</td>
 		<td>${book.auther}</td>
 		<td>${book.isbn}</td>
 		<td><a href='#' class='delete'>X</a></td>
-	`;
-	bookTable.appendChild(newBook);
+	`;   // ***
+	bookTable.appendChild(newBook);   // ***
 }
 UI.prototype.clearInput = function(params) {
 	document.getElementById('title').value = '';
 	document.getElementById('author').value = '';
 	document.getElementById('isbn').value = '';
+}
+
+// method to add the message div before the form
+UI.prototype.setAlertDiv = function(msg, cn) {
+	const messageDiv = document.createElement('div');
+	messageDiv.className=`alert ${cn}`;  // ***             
+	messageDiv.appendChild(document.createTextNode(msg));  // ***
+	console.log(messageDiv);
+
+	// insert message div into container before the form element.
+	const container = document.querySelector('.container');  // ***
+	const form = document.querySelector('#book-form');  // ***
+	container.insertBefore(messageDiv,form);  // ***
+
+	// set message time out, using windows.setTimeout
+	window.setTimeout(function() {   // ***
+		messageDiv.remove();
+	}, 3000);
 }
 
 // add submit event listener
@@ -34,8 +55,14 @@ document.getElementById('book-form').addEventListener('submit',
 
 	const book = new Book(inputTitle, inputAuthor, inputISBN);
 	const ui = new UI;
-	ui.addBookToTable(book);
-	ui.clearInput();
+	if (inputTitle === '' || inputAuthor === '' || inputISBN === '') {   // ***
+		ui.setAlertDiv('Please fill in all fields','error'); // ***
+	}
+	else{
+		ui.addBookToTable(book);
+		ui.setAlertDiv('Book Added.', 'success'); // ***
+		ui.clearInput();
+	}
 
 	e.preventDefault();
 });
